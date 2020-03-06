ec4fefa690142360c31dc3277ec4abe9642d1ad7
KOUSHIK SAHA <koushik.official1999@gmail.com>
2020-03-06 18:55:10 +0530

    changes detected

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,15 @@
 let age = 35;
+let year = 2020;
+console.log(age, year);
+age = 40;
 console.log(age);
+const points = 50;
+console.log(points);
+//points = 100;(error because points is constant)
+
+//String
+let fname = 'Koushik';
+console.log(fname)
+let lname = 'Saha';
+fname = fname+' '+lname;
+console.log(fname)
